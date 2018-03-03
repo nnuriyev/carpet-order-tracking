e11f49a7a35585d8b3c16d4ab0bf081c3e754f64
Nurlan Nuriyev <nnuriyev6@gmail.com>
2018-03-03 23:42:26 +0400

    Changed migration class name

--- database/migrations/2018_03_03_184802_update_orders_table_three.php
+++ database/migrations/2018_03_03_184802_update_orders_table_three.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateOrderTableThree extends Migration
+class UpdateOrdersTableThree extends Migration
 {
     /**
      * Run the migrations.
